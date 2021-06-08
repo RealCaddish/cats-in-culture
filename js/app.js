6e5dd346fbc064fc0c9be19a66b28755cd677f7a
deatonnr <nathaniel.deaton@gmail.com>
2021-06-08 14:16:20 -0400

    updates to html, js, and css
    Added style for map in css, reformatted geojson of cat_sites, added map div element, cdn call to Leaflet library, map and tile functions in js.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function () {
+
+  // Leaflet map creation
+  const map = L.map('map').setView([35.852, 21.443], 2);
+
+  const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+    maxZoom: 16
+  });
+
+}
+)
