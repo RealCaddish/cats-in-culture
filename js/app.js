9643b5dce17cbebdbe0c67ee27ca29767b395481
deatonnr <nathaniel.deaton@gmail.com>
2021-06-14 17:09:43 -0400

    styles, bootstrap, makeSlide example
    added styles, bootstrap for DOM, and put together a basic example of the slides control. Also attempted to add 'historyBox' control to the map that will dynamically update with the description content. This will need to be added to button events.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@
     position: 'bottomleft'
   }).addTo(map)
 
+  // add basemap tiles layer 
   const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
     maxZoom: 16
@@ -25,19 +26,31 @@
   tiles.addTo(map);
 
   // new function - make slide
+  makeSlide(15)
+
+  function makeSlide(slideNum) {
+  // data call for territories polygons 
+  const ter = $.getJSON("../data/territories_joined2.geojson", 
+  function (data) {
+
+    L.geoJson(data, {
+      filter: function (feature) {
+        if (slideNum == feature.properties.Slide_number) {
+          return feature
+        }
+      }
+    }).addTo(map)
+  });
 
   // data call for the archaeological sites 
   $.getJSON("../data/cat_sites.geojson", function (data) {
-    
-    console.log
 
     // access the features 
-
     var archSlide = data.features
 
     console.log(archSlide)
 
-    var archSitesStyle = {
+    const archSitesStyle = {
       radius: 8,
       fillColor: "#ff7800",
       color: "#000",
@@ -47,81 +60,44 @@
     };
     // adding data as points to Leaflet map
     L.geoJson(data, {
+      filter: function (feature) {
+        if (slideNum == feature.properties.ID) {
+          return feature
+        }
+      },
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, archSitesStyle); // creating layer with circleMarkers and adding custom options
       }
-
-      // filter the data by slide ID 
-      // filter: function(data) {
-      // return: L.circleMarker
-      // }
     }).addTo(map)
-    map.getBounds(), {
-      padding: [18, 18] // add padding around the archaeological sites
-    };
-    // replace drawMap 
-    //drawMap(data);
+  })
 
-  });
+  const zoom = 10
+  $.getJSON("../data/cat_slides.geojson", function (data) {
+    for (const x of data.features) {
+      if (slideNum == x.properties.Slide) {
+        console.log(x.properties)
+        console.log([x.properties.Lat, x.properties.Lon])
+        map.setView([x.properties.Lat, x.properties.Lon], zoom)
+      }
+    }
+  })
 
-  // data call for territories polygons 
-  $.getJSON("../data/territories_joined2.geojson", function (data) {
+// dynamically update text box with historic contents 
+// add box to map 
+// const storyControl = L.control({
+//   position: 'bottomright'
+// });
 
-    console.log(data)
-    drawMap(data)
-  });
+// storyControl.onAdd = function (map) {
+  
+//   const controls = L.DomUtil.get("historyBox")
 
-  // draw the map 
-  function drawMap(data) {
+//   L.DomEvent.disableScrollPropagation(controls)
+//   L.DomEvent.disableClickPropagation(controls)
 
+//   return controls
+// }
+// storyControl.addTo(map)
+};
 
-    const territories = L.geoJson(data, {
-      // add hover and touch functionality to each feature layer
-
-      onEachFeature: function (feature, layer) {
-        //console.log(layer)
-        //when mousing over a layer 
-        layer.on('mouseover', function () {
-          //change stroke color and bring element to front
-          layer.setStyle({
-            color: '#ff6e02'
-          }).bringToFront();
-        });
-
-        // mouse off the layer to reset 
-        layer.on('mouseout', function () {
-          //reset style 
-          layer.setStyle({
-            color: '#20282e'
-          });
-        });
-      },
-    }).addTo(map)
-    map.fitBounds(territories.getBounds(), {
-      padding: [18, 18] // add padding around the archaeological sites
-    });
-
-
-
-    //Leaflet control for title 
-    //   L.Control.title = L.Control.extend({
-    //     onAdd: function (map) {
-    //       var title = L.DomUtil.create('div');
-    //       title.id = "title";
-    //       text.innerHTML = "<strong> Cats in Culture </strong>"
-    //       return title
-    //     },
-    //     onRemove: function (map) {
-
-    //       // nothing to do here
-    //     }
-
-    //   });
-    //   L.Control.title = function (opts) {
-
-    //     return new L.Control.title(opts);
-    //   }
-
-    //   L.control.title({ position: 'bottomleft' }).addTo(map);
-  }
 })();
