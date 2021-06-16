76e118f45e80631d5f55d995b9ee8e5b0aaf2813
deatonnr <nathaniel.deaton@gmail.com>
2021-06-16 12:15:48 -0400

    formatting update

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
     position: 'bottomleft'
   }).addTo(map)
 
-  
+
 
   // add basemap tiles layer 
   const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
@@ -28,88 +28,65 @@
   tiles.addTo(map);
 
 
-  const slide = 22; 
+  const slide = 22;
 
   // // new function - make slide
   makeSlide(slide)
 
   function makeSlide(slideNum) {
-  // data call for territories polygons 
-  const ter = $.getJSON("../data/territories_joined2.json", 
-  function (data) {
+    // data call for territories polygons 
+    const ter = $.getJSON("../data/territories_joined2.json",
+      function (data) {
 
-    L.geoJson(data, {
-      filter: function (feature) {
-        if (slideNum == feature.properties.Slide_number) {
-          return feature
+        L.geoJson(data, {
+          filter: function (feature) {
+            if (slideNum == feature.properties.Slide_number) {
+              return feature
+            }
+          }
+        }).addTo(map)
+      });
+
+    // data call for the archaeological sites 
+    $.getJSON("../data/cat_sites.geojson", function (data) {
+
+      // access the features 
+      var archSlide = data.features
+
+      console.log(archSlide)
+
+      const archSitesStyle = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      };
+      // adding data as points to Leaflet map
+      L.geoJson(data, {
+        filter: function (feature) {
+          if (slideNum == feature.properties.Slide_ID) {
+            return feature
+          }
+        },
+        pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, archSitesStyle); // creating layer with circleMarkers and adding custom options
+        }
+      }).addTo(map)
+    })
+
+    $.getJSON("../data/cat_slides.geojson", function (data) {
+
+      console.log(data)
+      for (const x of data.features) {
+        if (slideNum == x.properties.Slide) {
+          const zoom = x.properties.Zoom
+          console.log(x.properties)
+          console.log([x.properties.Lat, x.properties.Lon])
+          map.setView([x.properties.Lat, x.properties.Lon], zoom)
         }
       }
-    }).addTo(map)
-  });
-
-  // data call for the archaeological sites 
-  $.getJSON("../data/cat_sites.geojson", function (data) {
-
-    // access the features 
-    var archSlide = data.features
-
-    console.log(archSlide)
-
-    const archSitesStyle = {
-      radius: 8,
-      fillColor: "#ff7800",
-      color: "#000",
-      weight: 1,
-      opacity: 1,
-      fillOpacity: 0.8
-    };
-    // adding data as points to Leaflet map
-    L.geoJson(data, {
-      filter: function (feature) {
-        if (slideNum == feature.properties.Slide_ID) {
-          return feature
-        }
-      },
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, archSitesStyle); // creating layer with circleMarkers and adding custom options
-      }
-    }).addTo(map)
-  })
-
-  $.getJSON("../data/cat_slides.geojson", function (data) {
-
-    console.log(data)
-    for (const x of data.features) {
-      if (slideNum == x.properties.Slide) {
-        const zoom = x.properties.Zoom
-        console.log(x.properties)
-        console.log([x.properties.Lat, x.properties.Lon])
-        map.setView([x.properties.Lat, x.properties.Lon], zoom)
-      }
-    }
-  })
-
-// dynamically update text box with historic contents 
-// add box to map 
-// const storyControl = L.control({
-//   position: 'bottomright'
-// });
-
-// storyControl.onAdd = function (map) {
-  
-//   const controls = L.DomUtil.get("historyBox")
-
-//   L.DomEvent.disableScrollPropagation(controls)
-//   L.DomEvent.disableClickPropagation(controls)
-
-//   return controls
-// }
-// storyControl.addTo(map)
-
+    })
   }
-
-
-// iterate through slides 
-
-
 })();
